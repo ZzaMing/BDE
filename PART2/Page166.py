8da19e3f7c028903eb1fbcfda8970401b5ba3330
ZzaMing <whaudgus123@naver.com>
2025-05-07 01:52:16 +0900

    5/7

--- PART2/Page166.py
+++ PART2/Page166.py
@@ -1,12 +1,53 @@
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/YoungjinBD/data/main/bike_data.csv')
+df = pd.read_csv("https://raw.githubusercontent.com/YoungjinBD/data/main/bike_data.csv")
 
 # print(df.info())
 
-df = df.astype({'datetime': 'datetime64[ns]',
-                'weather' : 'int64',
-                'season': 'object',
-                'workingday': 'object',
-                'holiday': 'object'})
+df = df.astype(
+    {
+        "datetime": "datetime64[ns]",
+        "weather": "int64",
+        "season": "object",
+        "workingday": "object",
+        "holiday": "object",
+    }
+)
 
+# df_sub = df.loc[df["season"] == 1]
+# df_sub["hour"] = df_sub["datetime"].dt.hour
+# summary_data = df_sub.groupby(["season", "hour"]).agg({"count": "sum"}).reset_index()
+# max_count_hour = summary_data.loc[summary_data["count"].idxmax(), "hour"]
+# print(max_count_hour)
+
+# season_avg = df.groupby('season')['count'].mean()
+# print(season_avg)
+
+# groupmonth = df.groupby(df['datetime'].dt.month).agg({"count": 'sum'}).reset_index()
+# print(groupmonth.loc[groupmonth['datetime'] == 1 , 'count'])
+# result = df[df['datetime'].dt.month == 1]['count'].sum()
+# print(result)
+
+# df['date'] = df['datetime'].dt.date
+# gro_df = df.groupby('date')['count'].sum().reset_index()
+# print(gro_df['count'].max())
+
+# df['hour'] = df['datetime'].dt.hour
+# result = df.groupby('hour')['count'].mean().reset_index()
+# print(result)
+
+# df['weekday'] = df['datetime'].dt.weekday
+# result = df[df["weekday"] == 0]['count'].sum()
+# print(result)
+
+# melted_df = df.melt(id_vars=['datetime', 'season'],
+#                  value_vars=['casual', 'registered'],
+#                  var_name='user_type',
+#                  value_name='total_count')
+# result = (
+#     melted_df
+#     .groupby(['season','user_type'])['total_count']
+#     .mean()
+#     .reset_index()
+# )
+# print(result)
