a0ce47286ef058c875e54a5293528bcb69fc2437
zzaming <whaudgus123@naver.com>
2025-05-22 16:07:36 +0900

    5/22

--- PART3/Page244.py
+++ PART3/Page244.py
@@ -55,13 +55,38 @@ test_X_preprocessed = np.concatenate(
 
 # 4. 모델 적합 / 기본 성능 보장되는 랜덤포레스트 사용
 from sklearn.ensemble import RandomForestRegressor
+
 rf = RandomForestRegressor(random_state=1)
 rf.fit(train_X_preprocessed, train_y)
 RandomForestRegressor(random_state=1)
 
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+
 pred_val = rf.predict(valid_X_preprocessed)
-print('valid RMSE: ', mean_squared_error(valid_y, pred_val))
-# print('valid RMSE: ', mean_squared_error(valid_y, pred_val, squared=False))
+# print('valid RMSE: ', mean_squared_error(valid_y, pred_val))
+print("valid RMSE: ", root_mean_squared_error(valid_y, pred_val))
+
 
 # 5. 테스트 데이터로 예측
+test_pred = rf.predict(test_X_preprocessed)
+test_pred = pd.DataFrame(test_pred, columns=["pred"])
+# test_pred.to_csv('result.csv', index = False)
+
+# 6. 하이퍼파라미터 튜닝
+train_X_full = np.concatenate([train_X_preprocessed, valid_X_preprocessed], axis=0)
+train_y_full = np.concatenate([train_y, valid_y], axis=0)
+
+from sklearn.model_selection import GridSearchCV
+
+param_grid = {"max_depth": [10, 20, 30], "min_samples_split": [2, 5, 10]}
+rf = RandomForestRegressor(random_state=1)
+rf_search = GridSearchCV(estimator= rf,
+                         param_grid= param_grid,
+                         cv = 3,
+                         scoring= 'neg_root_mean_squared_error')
+rf_search.fit(train_X_full, train_y_full)
+print('교차검증 RMSE: ', -rf_search.best_score_)
+
+test_pred2 = rf_search.predict(test_X_preprocessed)
+test_pred2 = pd.DataFrame(test_pred2, columns= ['pred'])
+# test_pred2.to_csv('result.csv', index=False)
